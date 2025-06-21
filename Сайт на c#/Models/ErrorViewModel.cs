b6de0719e011367de593388125b18a5f82412022
3akatik2009 <160848188+3akatik2009@users.noreply.github.com>
2025-06-21 20:35:37 +0300

    Добавьте файлы проекта.

---
+++ Сайт на c#/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Сайт_на_c_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
