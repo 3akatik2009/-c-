4c36ff3c7753e2316154062743afec87f8d14caa
3akatik2009 <160848188+3akatik2009@users.noreply.github.com>
2025-06-28 15:32:37 +0300

    перенес оставшиимоделиеся

--- Сайт на c#/Controllers/ContactController.cs
+++ Сайт на c#/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Сайт_на_c_.Models;
 
 namespace Сайт_на_c_.Controllers
 {
@@ -7,6 +8,14 @@ namespace Сайт_на_c_.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Check(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
         }
     }
 }
--- Сайт на c#/Models/Contact.cs
+++ Сайт на c#/Models/Contact.cs
@@ -2,7 +2,7 @@
 {
     public class Contact
     {
-        string Name { get; set; }
+         public string Name { get; set; }
         public string Surname { get; set; }
         public int Age { get; set; }
         public string Email { get; set; }
